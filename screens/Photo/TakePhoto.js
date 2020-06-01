fad306e2d72a1a57b02cae460e9c9b7c8a9ef8da
Kwon770 <sckwon770@gmail.com>
2020-06-02 07:55:59 +0900

    18-5 Saving photo

--- screens/Photo/TakePhoto.js
+++ screens/Photo/TakePhoto.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { TouchableOpacity, Platform } from "react-native";
 import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
 import { Ionicons } from "@expo/vector-icons";
 import * as Permissions from "expo-permissions";
 import styled from "styled-components";
@@ -11,13 +12,40 @@ import styles from "../../styles";
 const View = styled.View`
   flex: 1;
   background-color: white;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Button = styled.View`
+  width: 80px;
+  height: 80px;
+  border-radius: 40px;
+  border: 10px solid ${styles.lightGrayColor};
 `;
 
 export default ({ navigation }) => {
+  const cameraRef = useRef();
+  const [canTakePhoto, setCanTakePhoto] = useState(true);
   const [loading, setLoading] = useState(true);
   const [hasPermissions, setHasPermissions] = useState(false);
   const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
 
+  const takePhoto = async () => {
+    if (!canTakePhoto) {
+      return;
+    }
+
+    try {
+      setCanTakePhoto(false);
+      const { uri } = await cameraRef.current.takePictureAsync({
+        quality: 1,
+      });
+      await MediaLibrary.createAssetAsync(uri);
+    } catch (e) {
+      console.log(e);
+      setCanTakePhoto(true);
+    }
+  };
   const askPermission = async () => {
     try {
       const { status } = await Permissions.askAsync(Permissions.CAMERA);
@@ -47,27 +75,35 @@ export default ({ navigation }) => {
       {loading ? (
         <Loader />
       ) : hasPermissions ? (
-        <Camera
-          type={cameraType}
-          style={{
-            width: constants.width,
-            height: constants.height / 2,
-            justifyContent: "flex-end",
-            padding: 20,
-          }}
-        >
-          <TouchableOpacity onPress={toggleCameraType}>
-            <Ionicons
-              name={
-                Platform.OS === "ios"
-                  ? "ios-reverse-camera"
-                  : "md-reverse-camera"
-              }
-              size={28}
-              color={styles.lightGrayColor}
-            />
-          </TouchableOpacity>
-        </Camera>
+        <>
+          <Camera
+            ref={cameraRef}
+            type={cameraType}
+            style={{
+              width: constants.width,
+              height: constants.height / 2,
+              justifyContent: "flex-end",
+              padding: 20,
+            }}
+          >
+            <TouchableOpacity onPress={toggleCameraType}>
+              <Ionicons
+                name={
+                  Platform.OS === "ios"
+                    ? "ios-reverse-camera"
+                    : "md-reverse-camera"
+                }
+                size={28}
+                color={styles.lightGrayColor}
+              />
+            </TouchableOpacity>
+          </Camera>
+          <View>
+            <TouchableOpacity onPress={takePhoto} disabled={!canTakePhoto}>
+              <Button />
+            </TouchableOpacity>
+          </View>
+        </>
       ) : null}
     </View>
   );
